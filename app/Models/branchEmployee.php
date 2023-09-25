5d5b16425fea90d71a82fdd285ae3016796b77dd
Shekh Aatif <aatif.codeflix@gmail.com>
2023-09-25 11:34:34 +0530

    changes

--- app/Models/branchEmployee.php
+++ app/Models/branchEmployee.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class branchEmployee extends Model
+class BranchEmployee extends Model
 {
     use HasFactory;
 }
